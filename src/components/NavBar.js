63f6192c1d8e3a6c583c719033c1e780dfeae148
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-10 22:34:56 -0300

    mas mmmm

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { MenuItems } from "./MenuItems"
+import "./NavBar.css"
 
 class NavBar extends React.Component {
     render(){
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">Pau App</h1>
+                <h1 className="navbar-logo">Samuel<i className="Cat"></i></h1>
                 <div className="menu-icon">
                     
                 </div>
