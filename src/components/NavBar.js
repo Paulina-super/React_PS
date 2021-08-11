2536c9b6d4f5d31f89bcdc72b277a20f9f5862e6
Paulina-super <56806110+Paulina-super@users.noreply.github.com>
2021-08-10 21:58:45 -0300

    A ver si con esto va

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,20 @@
-import { render } from '@testing-library/react';
 import React from 'react';
-import ".styles.css";
+import { MenuItems } from "./MenuItems"
 
 class NavBar extends React.Component {
     render(){
         return(
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div class="navbar-nav">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                <a class="nav-link" href="#">Features</a>
-                                <a class="nav-link" href="#">Pricing</a>
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Pau App</h1>
+                <div className="menu-icon">
+                    
+                </div>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
         )
-
     }
 }
 
 export default NavBar
-
